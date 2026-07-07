14cf1e6a83e4d9b9b6dda844158477c561e89650
junbo.dai <bobodai515@163.com>
2026-07-07 22:11:21 +0800

    i commit

--- exercises/04_static/main.cpp
+++ exercises/04_static/main.cpp
@@ -5,15 +5,15 @@
 static int func(int param) {
     static int static_ = param;
     // std::cout << "static_ = " << static_ << std::endl;
-    return static_++;
+    return ++static_;
 }
 
 int main(int argc, char **argv) {
     // TODO: 将下列 `?` 替换为正确的数字
-    ASSERT(func(5) == ?, "static variable value incorrect");
-    ASSERT(func(4) == ?, "static variable value incorrect");
-    ASSERT(func(3) == ?, "static variable value incorrect");
-    ASSERT(func(2) == ?, "static variable value incorrect");
-    ASSERT(func(1) == ?, "static variable value incorrect");
+    ASSERT(func(5) == 6, "static variable value incorrect");
+    ASSERT(func(4) == 7, "static variable value incorrect");
+    ASSERT(func(3) == 8, "static variable value incorrect");
+    ASSERT(func(2) == 9, "static variable value incorrect");
+    ASSERT(func(1) == 10, "static variable value incorrect");
     return 0;
 }
--- exercises/16_class_move/main.cpp
+++ exercises/16_class_move/main.cpp
@@ -15,21 +15,39 @@ class DynFibonacci {
 
 public:
     // TODO: 实现动态设置容量的构造器
-    DynFibonacci(int capacity): cache(new ?), cached(?) {}
+    DynFibonacci(int capacity): cache(new size_t[capacity]), cached(0) {
+        cached = 2;
+        cache[0] = 0;
+        cache[1] = 1;
+    }
 
     // TODO: 实现移动构造器
-    DynFibonacci(DynFibonacci &&) noexcept = delete;
+    DynFibonacci(DynFibonacci &&other) noexcept : cache(other.cache), cached(other.cached) {
+        other.cache = nullptr;
+        other.cached = 0;
+    }
 
     // TODO: 实现移动赋值
     // NOTICE: ⚠ 注意移动到自身问题 ⚠
-    DynFibonacci &operator=(DynFibonacci &&) noexcept = delete;
+    DynFibonacci &operator=(DynFibonacci &&other) noexcept {
+        if (this != &other) {
+            delete[] cache; // 释放当前对象的资源
+            cache = other.cache; // 接管资源
+            cached = other.cached; // 更新缓存计数
+            other.cache = nullptr; // 将源对象置为空，防止析构时释放资源
+            other.cached = 0; // 重置源对象的缓存计数
+        }
+        return *this;
+    }
 
     // TODO: 实现析构器，释放缓存空间
-    ~DynFibonacci();
+    ~DynFibonacci() {
+        delete[] cache;
+    }
 
     // TODO: 实现正确的缓存优化斐波那契计算
     size_t operator[](int i) {
-        for (; false; ++cached) {
+        for (; this->cached <= i; ++cached) {
             cache[cached] = cache[cached - 1] + cache[cached - 2];
         }
         return cache[i];
